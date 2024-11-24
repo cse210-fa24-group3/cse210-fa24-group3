7e05e309df991ca9f0f1bfa5e27a4988b9ff7287
Aryan Dokania <aryandokania2001@gmail.com>
2024-11-23 19:53:57 -0800

    add

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,14 +1,15 @@
 module.exports = {
     testEnvironment: "jest-environment-jsdom", // Required for DOM-based tests
-    // roots: ["<rootDir>/src"],
-    testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
-    testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-    collectCoverage: true,
-    collectCoverageFrom: [
-      "/**/*.{js,jsx}",
-      "/**/*.d.ts",
-      "/__tests__/**",
-    ],
+    roots: ["<rootDir>/"],
+    testMatch: ["**/__tests__/**/*.[jt]s?(x)"], // Matches all test files in __tests__
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"], // Ignore node_modules and dist folders
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/__tests__/**/*.[jt]s?(x)", // Collect coverage only from test files in __tests__
+  ],
+  coveragePathIgnorePatterns: [
+    "/home.js$", // Ignore home.js from coverage
+  ],
     coverageDirectory: "<rootDir>/coverage",
     reporters: [
       "default",
@@ -17,8 +18,8 @@ module.exports = {
         {
           outputDirectory: "reports", // Directory for JUnit XML report
           outputName: "junit.xml", // File name for the report
-          classNameTemplate: "__tests__/home.test.js", // Use file path as classname
-          titleTemplate: "renders home component", // Use test name as the title
+          classNameTemplate: "{filepath}", // Use file path as classname
+          titleTemplate: "{title}", // Use test name as the title
         },
       ],
     ],
