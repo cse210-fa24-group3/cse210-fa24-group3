572d3a604c0eb0d1bd30330a878d488ea66bdd89
venu-c1 <venu.c@somaiya.edu>
2024-12-07 20:05:54 -0800

    Pages Integration

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:3000', // or the exact URL of your frontend
+    origin: 'http://localhost:3000', 
     credentials: true
 }));
 
